26358afbcfb971175a7c35739cd00f285dff231a
Shima <guilherme.shima91@hotmail.com>
2020-04-19 16:07:22 -0300

    first commit

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,37 @@
+import { Router, Request, Response } from 'express';
+import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+const appointmentsRouter = Router();
+
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+const appointments: Appointment[] = [];
+
+appointmentsRouter.post('/', (request: Request, response: Response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This Appointment is already booked' });
+  }
+  const appointment = {
+    id: uuid(),
+    provider,
+    date: parsedDate,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
+});
+export default appointmentsRouter;
